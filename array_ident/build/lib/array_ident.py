ab3f0a213e07396d4af4b5656923b2b32d7f301e
Gustavo Sá <gsbadbr@gmail.com>
2018-03-27 01:29:00 -0300

    Versão 1.2.0 do identador de códigos com controle de argumentos

--- array_ident/build/lib/array_ident.py
+++ array_ident/build/lib/array_ident.py
@@ -1,11 +1,19 @@
 """Função que varre todos os indices de um array e retorna seus valores identados"""
-def array_ident(lista):
+def array_ident(lista, ident=False, level=0):
     for indice in lista:
         #Verifica se o argumento inserido é uma lista
         if isinstance(indice, list):
             # Retorna a função recursivamente
-            array_ident(indice)
+            array_ident(indice, ident, level+1)
         else:
-            # Imprime o item
+            if ident:
+                for tab_stop in range(level):
+                    print("\t", end='')
             print(indice)
-print("importado!")
+
+""" 
+############ EXEMPLO DE USO ############
+
+movies = ["The holy grail", 1975, "Terry Jones", 91,["Graham Chapman",["Michael","Terry","Joe","Bulba"]]]
+
+"""
